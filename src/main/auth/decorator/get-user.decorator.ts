fdb84bce9651debebcfbb4edfdefb5acc4e79e3a
ranamiah221 <ranarasul21@gmail.com>
2026-01-21 06:01:23 +0600

    added custom decorator

--- src/main/auth/decorator/get-user.decorator.ts
+++ src/main/auth/decorator/get-user.decorator.ts
@@ -9,20 +9,16 @@ interface UserPayload {
 }
 
 interface RequestWithUser extends Request {
-  user: UserPayload;
+  user?: UserPayload;
 }
 
 export const GetUser = createParamDecorator(
-  (
-    data: keyof UserPayload | undefined,
-    ctx: ExecutionContext,
-  ): string | UserPayload | null => {
+  (data: keyof UserPayload | undefined, ctx: ExecutionContext): any => {
     const request = ctx.switchToHttp().getRequest<RequestWithUser>();
-    if (!request.user) return null;
+    const user = request.user;
+    if (!user) return null;
 
-    if (data && data in request.user) {
-      return request.user[data] as string;
-    }
-    return request.user;
+    if (data === 'id') return user.sub; // alias
+    return data ? user[data] : user;
   },
 );
