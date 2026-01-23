1252b53beb06143270551a6d53dd8380eef5c6d2
ranamiah221 <ranarasul21@gmail.com>
2026-01-24 04:04:23 +0600

    profile get update

---
+++ src/main/user/profile/dto/update.profile.dto.ts
@@ -0,0 +1,20 @@
+import { ApiPropertyOptional, PickType, PartialType } from '@nestjs/swagger';
+import { IsEnum, IsOptional } from 'class-validator';
+
+import { CreateProfileDto } from './create.profile.dto';
+import { AccountType } from 'generated/prisma/enums';
+
+export class UpdateProfileBaseDto extends PartialType(
+  PickType(CreateProfileDto, [
+    'userName',
+    'bio',
+    'imageUrl',
+    'instagramHandler',
+    'accountType',
+  ] as const),
+) {
+  @ApiPropertyOptional({ enum: AccountType, example: 'PUBLIC' })
+  @IsOptional()
+  @IsEnum(AccountType)
+  accountType?: AccountType;
+}
