7829690d0e3d0660b7fc98efe1384c73c375bae0
ranamiah221 <ranarasul21@gmail.com>
2026-01-24 04:04:23 +0600

    profile get update

---
+++ src/main/user/profile/dto/changed.profile.type.dto.ts
@@ -0,0 +1,35 @@
+import {
+  ApiProperty,
+  ApiPropertyOptional,
+  IntersectionType,
+  PickType,
+  PartialType,
+} from '@nestjs/swagger';
+
+import { CreateProfileDto } from './create.profile.dto';
+import { Type as ProfileType } from 'generated/prisma/enums';
+import { IsEnum } from 'class-validator';
+
+class ChangeTypeRequiredDto extends PickType(CreateProfileDto, [
+  'profileType',
+] as const) {
+  @ApiProperty({ enum: ProfileType, example: 'PRO_BUSSINESS' })
+  @IsEnum(ProfileType)
+  profileType!: ProfileType;
+}
+
+class ChangeTypeOptionalPayloadDto extends PartialType(
+  PickType(CreateProfileDto, [
+    'spotter',
+    'owner',
+    'creator',
+    'business',
+    'proDriver',
+    'simRacing',
+  ] as const),
+) {}
+
+export class ChangeProfileTypeDto extends IntersectionType(
+  ChangeTypeRequiredDto,
+  ChangeTypeOptionalPayloadDto,
+) {}
