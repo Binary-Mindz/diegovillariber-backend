0e30a965189819f026873f94aa0aedb814fb607e
ranamiah221 <ranarasul21@gmail.com>
2026-01-31 02:58:47 +0600

    save post implement successfully

---
+++ src/main/posts/save/dto/unsave-post.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsUUID } from 'class-validator';
+
+export class UnsavePostDto {
+  @ApiProperty({ example: '7e3525f6-a27b-41db-bdaa-e80bd4877af4' })
+  @IsUUID()
+  postId: string;
+}
