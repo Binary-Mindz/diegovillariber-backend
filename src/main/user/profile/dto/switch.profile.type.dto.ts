01335d1b9ec416bd5975b7e9ea83fbb8cbc28f4a
ranamiah221 <ranarasul21@gmail.com>
2026-02-21 04:16:25 +0600

    added legal notice

--- src/main/user/profile/dto/switch.profile.type.dto.ts
+++ src/main/user/profile/dto/switch.profile.type.dto.ts
@@ -1,9 +1,11 @@
 // dto/switch-profile-type.dto.ts
 
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum } from 'class-validator';
 import { Type } from 'generated/prisma/enums';
 
 export class SwitchProfileTypeDto {
+   @ApiPropertyOptional({ enum: Type, example: Type.SPOTTER })
   @IsEnum(Type)
   type: Type;
 }
