c4cbec2d194afc888fe37cc533b232ee33f67a53
ranamiah221 <ranarasul21@gmail.com>
2026-02-28 23:17:49 +0600

    admin seeding successful

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaService } from './common/prisma/prisma.service';
+import { seedAdmin } from './common/seed/admin.seed';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +17,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  if (process.env.AUTO_SEED === 'true') {
+    const prisma = app.get(PrismaService);
+
+    console.log('AUTO_SEED enabled. Seeding admin...');
+    await seedAdmin(prisma);
+    console.log('Seeding done ✅');
+  }
 
   const config = new DocumentBuilder()
     .setTitle('Diegovillariber Backend Server')
