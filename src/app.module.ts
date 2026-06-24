f83a02d688af9baacfce5bd29eb175fa5cd6fb1c
ranamiah221 <ranarasul21@gmail.com>
2026-06-24 20:24:59 +0600

    added lang

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,11 +19,34 @@ import { SpottingRequestModule } from './main/sportting-request/sportting-reques
 import { DiscoverModule } from './main/discover/discover.module';
 import { MapModule } from './main/map/map.module';
 import { FirebaseModule } from './common/firebase/firebase.module';
+import * as path from 'path';
+import { HeaderResolver, I18nModule } from 'nestjs-i18n';
+import * as fs from 'fs';
+
+const getI18nPath = () => {
+  const devPath = path.join(process.cwd(), 'src', 'i18n');
+  if (fs.existsSync(devPath)) return devPath;
+
+  const prodPath = path.join(process.cwd(), 'dist', 'src', 'i18n');
+  if (fs.existsSync(prodPath)) return prodPath;
+
+  return path.join(process.cwd(), 'i18n');
+};
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: getI18nPath(), // এখানে ডাইনামিক পাথটি কল করা হলো
+        watch: true,
+      },
+      resolvers: [
+        new HeaderResolver(['accept-language']),
+      ],
+    }),
     PrismaModule,
     DiscoverModule,
     AuthModule,
@@ -46,4 +69,4 @@ import { FirebaseModule } from './common/firebase/firebase.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
