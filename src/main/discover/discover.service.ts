02339b9bfb00917bddc507474aeb7757c6c14f2a
azmain-siam <siamhossain2700@gmail.com>
2026-07-08 21:26:21 +0600

    fix: hashtag issue

--- src/main/discover/discover.service.ts
+++ src/main/discover/discover.service.ts
@@ -144,7 +144,7 @@ export class DiscoverService {
       challengeSubmissions: {
         some: {
           hashtags: {
-            has: `#${normalizedKeyword}`,
+            has: `${normalizedKeyword}`,
           }
         }
       },
