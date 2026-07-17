e5a4011b31eb836251525176cbc56be3ad58574e
azmain-siam <siamhossain2700@gmail.com>
2026-07-17 22:12:38 +0600

    fix: some issues

--- src/main/program/head-to-head/head-to-head.module.ts
+++ src/main/program/head-to-head/head-to-head.module.ts
@@ -1,9 +1,11 @@
+import { NotificationModule } from '@/main/notification/notification.module';
 import { Module } from '@nestjs/common';
 import { HeadToHeadController } from './head-to-head.controller';
-import { HeadToHeadService } from './head-to-head.service';
 import { HeadToHeadBattleCron } from './head-to-head.cron';
+import { HeadToHeadService } from './head-to-head.service';
 
 @Module({
+  imports: [NotificationModule],
   controllers: [HeadToHeadController],
   providers: [HeadToHeadService, HeadToHeadBattleCron],
   exports: [HeadToHeadService],
--- src/main/program/raw-shift/rawshift.module.ts
+++ src/main/program/raw-shift/rawshift.module.ts
@@ -1,12 +1,14 @@
+import { PrismaService } from '@/common/prisma/prisma.service';
+import { NotificationModule } from '@/main/notification/notification.module';
+import { NotificationService } from '@/main/notification/notification.service';
 import { Module } from '@nestjs/common';
 import { RawShiftController } from './rawshift.controller';
 import { RawShiftService } from './rawshift.service';
-import { PrismaService } from '@/common/prisma/prisma.service';
-
 
 @Module({
+  imports: [NotificationModule],
   controllers: [RawShiftController],
-  providers: [RawShiftService, PrismaService],
+  providers: [RawShiftService, PrismaService, NotificationService],
   exports: [RawShiftService],
 })
 export class RawShiftModule {}
